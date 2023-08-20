2d49d76ab53957d16f8f4dc8317a32e5c5f03d27
Vaalley <vaaleyou@gmail.com>
2023-08-20 22:18:14 +0200

    Refactor code for generating YouTube description
    The commit refactors the code for generating the YouTube description by using a context manager for Playwright and optimizing the web scraping process.

--- tc_description_generator.py
+++ tc_description_generator.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 import urllib.parse
 
 
-# Function to copy the generated description to the clipboard
 def copy_to_clipboard():
     description = generated_description_text.get("1.0", tk.END)
     app.clipboard_clear()
@@ -13,47 +12,38 @@ def copy_to_clipboard():
     app.update()
 
 
-# Function to scrape and generate the YouTube description
 def scrape_and_generate_description():
     soundcloud_url = url_entry.get()
 
-    # Set up Playwright
     with sync_playwright() as p:
         browser = p.chromium.launch()
-        page = browser.new_page()
-
-        # Extract artist social links from artist page
         artist_page_url = soundcloud_url.split("/")[3]
         artist_url = f"https://soundcloud.com/{artist_page_url}"
-        page.goto(artist_url)
-        page.wait_for_selector(".web-profiles")
-        artist_social_links_html = page.inner_html(".web-profiles")
-
-        # Extract track title and artist name from track page
         track_page_url = soundcloud_url
-        page.goto(track_page_url)
-        page.wait_for_selector(".soundTitle__usernameTitleContainer")
-        track_info_html = page.inner_html(".soundTitle__usernameTitleContainer")
 
-        browser.close()
+        with browser.new_page() as page:
+            # Extract artist social links from artist page
+            page.goto(artist_url)
+            page.wait_for_selector(".web-profiles")
+            artist_social_links_html = page.inner_html(".web-profiles")
 
-        # Parse artist social links HTML
+            # Extract track title and artist name from track page
+            page.goto(track_page_url)
+            page.wait_for_selector(".soundTitle__usernameTitleContainer")
+            track_info_html = page.inner_html(".soundTitle__usernameTitleContainer")
+
         soup = BeautifulSoup(artist_social_links_html, "html.parser")
         social_links = soup.find_all("a", class_="web-profile")
 
-        # Parse track info HTML
         track_soup = BeautifulSoup(track_info_html, "html.parser")
         track_title = track_soup.find("h1", class_="soundTitle__title").text.strip()
         artist_name = track_soup.find("h2", class_="soundTitle__username").text.strip()
 
-        # Clear the text field
         generated_description_text.delete("1.0", tk.END)
 
-        # Generate description
         description = f"🔥 {track_title} 🔥\n"
         description += f"SoundCloud: {soundcloud_url}\n\n"
 
-        # Add Trap Cosmos links
         description += "👌 Trap Cosmos 👌\n"
         description += "Discord: https://discord.gg/3gkPHKy\n"
         description += "Instagram: https://www.instagram.com/TrapCosmos\n"
@@ -62,50 +52,36 @@ def scrape_and_generate_description():
 
         description += f"👌 {artist_name} 👌\n"
 
-        # Add artist social links
         for link in social_links:
             link_text = link.text.strip()
             link_href = link.get("href")
 
-            # Exclude email and mailto links
             if not link_href.startswith("mailto"):
-                # Remove gate.sc parameters from the link
-                clean_link = link_href.split("https://gate.sc?url=")[-1]
-
-                # Replace URL-encoded characters
                 clean_link = (
-                    urllib.parse.unquote(clean_link)
+                    urllib.parse.unquote(link_href.split("https://gate.sc?url=")[-1])
                     .replace("%2F", "/")
                     .replace("%3A", ":")
                 )
-
-                # Remove token at the end of the link
                 clean_link = clean_link.split("&token=")[0]
 
                 description += f"{link_text}: {clean_link}\n"
 
-        # Add YouTube link if available
         youtube_link = f"https://www.youtube.com/{artist_page_url}"
         description += f"YouTube: @{artist_page_url}\n" if artist_page_url else ""
 
-        # Add Unsplash background link placeholder
         description += "\n📷 Background 📷\n"
         description += "⚠️ ADD UNSPLASH BACKGROUND LINK HERE ⚠️\n\n"
 
-        # Add epilepsy warning
         description += "💫 Epilepsy Warning 💫\n"
         description += "The visuals accompanied with the track may trigger seizures to those with photosensitive epilepsy. Please be cautious."
 
-        # Insert the generated description into the text field
         generated_description_text.insert(tk.END, description)
 
 
-# Create the main application window
 app = tk.Tk()
 app.title("YouTube Description Generator")
 app.geometry("1280x800")
 
-# Add URL entry and generate button
 url_label = tk.Label(app, text="Enter SoundCloud Track URL:")
 url_label.pack(pady=10)
 
@@ -117,13 +93,10 @@ generate_button = tk.Button(
 )
 generate_button.pack(pady=10)
 
-# Add copy button
-copy_button = tk.Button(app, text="Copy Description", command=copy_to_clipboard)
+copy_button = tk.Button(app, text="Copy to Clipboard", command=copy_to_clipboard)
 copy_button.pack(pady=5)
 
-# Add generated description text field
 generated_description_text = scrolledtext.ScrolledText(app, wrap=tk.WORD)
 generated_description_text.pack(fill=tk.BOTH, expand=True, pady=10)
 
-# Start the main event loop
 app.mainloop()
